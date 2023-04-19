74739db78956b3e6c4564eef1e7cc321cd5779c2
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-04-19 21:30:38 +0530

    Added the Task Routes

---
+++ routes/taskRoutes.js
@@ -0,0 +1,16 @@
+// Import the Express module and create a new router object
+const express = require("express");
+const router = express.Router();
+
+// Import the task controller module and the authentication middleware module
+const taskController = require("../controllers/taskController");
+const authMiddleware = require("../config/authMiddleware");
+
+// Define routes for creating, getting, updating, and deleting tasks
+router.post("/", authMiddleware, taskController.createTask);
+router.get("/", authMiddleware, taskController.getTasks);
+router.put("/:taskId", authMiddleware, taskController.updateTask);
+router.delete("/:taskId", authMiddleware, taskController.deleteTask);
+
+// Export the router from the module
+module.exports = router;
