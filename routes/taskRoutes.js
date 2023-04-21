15036286f4887a2d3c8ede64e1f4b382844cf71e
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-04-21 20:51:38 +0530

    Added Routes for Admin

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,16 +2,45 @@
 const express = require("express");
 const router = express.Router();
 
-// Import the task controller module and the authentication middleware module
+// Import the task controller, admin task controller module and the authentication middleware module
 const taskController = require("../controllers/taskController");
-const authMiddleware = require("../config/authMiddleware");
+const adminTaskController = require("../controllers/adminTaskController");
+const { authMiddleware, adminMiddleware } = require("../config/authMiddleware");
 
-// Define routes for creating, getting, updating, and deleting tasks
+// Define routes for creating, getting, filtering, updating, and deleting tasks
 router.post("/", authMiddleware, taskController.createTask);
 router.get("/getAllTasks", authMiddleware, taskController.getTasks);
 router.get("/filterTask", authMiddleware, taskController.filterTask);
 router.put("/:taskId", authMiddleware, taskController.updateTask);
 router.delete("/:taskId", authMiddleware, taskController.deleteTask);
+
+// Define routes for assigning tasks to users and getting user and task data by Admin
+router.post(
+  "/assignExistingTask",
+  authMiddleware,
+  adminMiddleware,
+  adminTaskController.assignExistingTask
+);
+router.post(
+  "/assignNewTask",
+  authMiddleware,
+  adminMiddleware,
+  adminTaskController.assignNewTask
+);
+
+router.get(
+  "/getAllUsers",
+  authMiddleware,
+  adminMiddleware,
+  adminTaskController.getAllUsers
+);
+
+router.get(
+  "/getTask/:userId",
+  authMiddleware,
+  adminMiddleware,
+  adminTaskController.getTaskById
+);
 
 // Export the router from the module
 module.exports = router;
